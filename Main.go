b0368aaa677fc7a55fa2be437829e80bc3c6e6b5
NiclasHjortkjaer <32992549+NiclasHjortkjaer@users.noreply.github.com>
2021-09-14 13:13:28 +0200

    fix eat (#9)
    Co-authored-by: Niclas Søgaard Hjortkjær <nihj@itu.dk>

--- Main.go
+++ Main.go
@@ -5,34 +5,37 @@ import (
 	"strconv"
 )
 
+var philosophers []Philosopher
+
 func main() {
 
 	var Forks = createForks()
 
-	var phil1 = Philosopher{number: 1, left: &Forks[0], right: &Forks[1], outgoing: make(chan string), incoming: make(chan string)}
-	var phil2 = Philosopher{number: 2, left: &Forks[1], right: &Forks[2], outgoing: make(chan string), incoming: make(chan string)}
-	var phil3 = Philosopher{number: 3, left: &Forks[2], right: &Forks[3], outgoing: make(chan string), incoming: make(chan string)}
-	var phil4 = Philosopher{number: 4, left: &Forks[3], right: &Forks[4], outgoing: make(chan string), incoming: make(chan string)}
-	var phil5 = Philosopher{number: 5, left: &Forks[4], right: &Forks[0], outgoing: make(chan string), incoming: make(chan string)}
+	var phil0 = Philosopher{number: 0, left: &Forks[0], right: &Forks[1], outgoing: make(chan string), incoming: make(chan string)}
+	var phil1 = Philosopher{number: 1, left: &Forks[1], right: &Forks[2], outgoing: make(chan string), incoming: make(chan string)}
+	var phil2 = Philosopher{number: 2, left: &Forks[2], right: &Forks[3], outgoing: make(chan string), incoming: make(chan string)}
+	var phil3 = Philosopher{number: 3, left: &Forks[3], right: &Forks[4], outgoing: make(chan string), incoming: make(chan string)}
+	var phil4 = Philosopher{number: 4, left: &Forks[4], right: &Forks[0], outgoing: make(chan string), incoming: make(chan string)}
 
+	philosophers = []Philosopher{phil0, phil1, phil2, phil3, phil4}
+
+	go phil0.receiver()
 	go phil1.receiver()
 	go phil2.receiver()
 	go phil3.receiver()
 	go phil4.receiver()
-	go phil5.receiver()
 
 	for {
 		select {
-		case msg1 := <-phil1.outgoing:
+		case msg1 := <-phil0.outgoing:
 			if msg1 == "Eating" {
 				fmt.Println("phil1 eating:" + strconv.Itoa(phil1.timesEaten))
 				fmt.Println("Fork 1 times used: " + strconv.Itoa(phil1.left.timesUsed))
 				fmt.Println("Fork 5 times used: " + strconv.Itoa(phil1.right.timesUsed))
-
 			} else {
 
 			}
-		case msg2 := <-phil2.outgoing:
+		case msg2 := <-phil1.outgoing:
 			if msg2 == "Eating" {
 				fmt.Println("phil2 eating: " + strconv.Itoa(phil2.timesEaten))
 				fmt.Println("Fork 2 times used: " + strconv.Itoa(phil2.left.timesUsed))
@@ -41,7 +44,7 @@ func main() {
 
 			}
 
-		case msg3 := <-phil3.outgoing:
+		case msg3 := <-phil2.outgoing:
 			if msg3 == "Eating" {
 				fmt.Println("phil3 eating: " + strconv.Itoa(phil3.timesEaten))
 				fmt.Println("Fork 3 times used: " + strconv.Itoa(phil3.left.timesUsed))
@@ -50,30 +53,22 @@ func main() {
 
 			}
 
-		case msg4 := <-phil4.outgoing:
+		case msg4 := <-phil3.outgoing:
 			if msg4 == "Eating" {
 				fmt.Println("phil4 eating: " + strconv.Itoa(phil4.timesEaten))
 				fmt.Println("Fork 4 times used: " + strconv.Itoa(phil4.left.timesUsed))
 				fmt.Println("Fork 3 times used: " + strconv.Itoa(phil4.right.timesUsed))
 			} else {
-				if phil3.timesEaten > phil5.timesEaten {
-					phil5.eat()
-				} else {
-					phil3.eat()
-				}
+
 			}
 
-		case msg5 := <-phil5.outgoing:
+		case msg5 := <-phil4.outgoing:
 			if msg5 == "Eating" {
 				fmt.Println("phil5 eating: " + strconv.Itoa(phil5.timesEaten))
 				fmt.Println("Fork 5 times used: " + strconv.Itoa(phil5.left.timesUsed))
 				fmt.Println("Fork 4 times used: " + strconv.Itoa(phil5.right.timesUsed))
 			} else {
-				if phil4.timesEaten > phil1.timesEaten {
-					phil1.eat()
-				} else {
-					phil4.eat()
-				}
+
 			}
 
 		}
--- Philosophers.go
+++ Philosophers.go
@@ -19,11 +19,19 @@ func (p Philosopher) eat() {
 	p.left.Lock()
 	p.right.Lock()
 	p.outgoing <- "Eating"
+  
+	philosophers[p.number-1%5].incoming <- "LeftNotFree"
+	philosophers[p.number+1%5].incoming <- "RightNotFree"
+
+	p.outgoing <- "Philosopher " + strconv.Itoa(p.number) + " has eaten " + strconv.Itoa(p.timesEaten)
 	time.Sleep(time.Millisecond * 500)
 	p.timesEaten++
 	p.left.Unlock()
 	p.right.Unlock()
 	p.outgoing <- "Thinking"
+
+	philosophers[p.number-1%5].incoming <- "LeftFree"
+	philosophers[p.number+1%5].incoming <- "RightFree"
 }
 
 func (p Philosopher) receiver() {
@@ -40,5 +48,13 @@ func (p Philosopher) receiver() {
 		} else if start == "timesEaten" {
 			p.outgoing <- strconv.Itoa(p.timesEaten)
 		}
+
+		p.eatIfPossible()
+	}
+}
+
+func (p Philosopher) eatIfPossible() {
+	if p.leftFree && p.rightFree {
+		p.eat()
 	}
 }
