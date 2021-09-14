e859f32e1b3931e1e4412ecd6f0e86ae04bc82a8
Niclas Søgaard Hjortkjær <nihj@itu.dk>
2021-09-14 10:53:10 +0200

    create class skeleton

--- Philosophers.go
+++ Philosophers.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 type Philosopher struct {
 	timesEaten int
 	left       *Fork
@@ -7,4 +9,15 @@ type Philosopher struct {
 	number     int
 	outgoing   chan string
 	incoming   chan string
+}
+
+func (p Philosopher) eat() {
+	p.left.locker.Lock()
+	p.right.locker.Lock()
+	p.outgoing <- "Eating"
+	time.Sleep(time.Millisecond * 500)
+	p.timesEaten++
+	p.left.locker.Unlock()
+	p.right.locker.Unlock()
+	p.outgoing <- "Thinking"
 }
