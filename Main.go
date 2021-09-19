0abb6291390d5224542d1056898854a7b285f8c9
NiclasHjortkjaer <32992549+NiclasHjortkjaer@users.noreply.github.com>
2021-09-19 20:00:21 +0200

    version 1.0 (#13)
    Co-authored-by: Niclas Søgaard Hjortkjær <nihj@itu.dk>

--- Main.go
+++ Main.go
@@ -5,92 +5,77 @@ import (
 	"strconv"
 )
 
-var philosophers []Philosopher
-
 func main() {
 
-	var Forks = createForks()
+	var fork0 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+	var fork1 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+	var fork2 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+	var fork3 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+	var fork4 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
 
-	var phil0 = Philosopher{number: 0, left: &Forks[0], right: &Forks[1], outgoing: make(chan string), incoming: make(chan string), leftFree: true, rightFree: true}
-	var phil1 = Philosopher{number: 1, left: &Forks[1], right: &Forks[2], outgoing: make(chan string), incoming: make(chan string), leftFree: true, rightFree: true}
-	var phil2 = Philosopher{number: 2, left: &Forks[2], right: &Forks[3], outgoing: make(chan string), incoming: make(chan string), leftFree: true, rightFree: true}
-	var phil3 = Philosopher{number: 3, left: &Forks[3], right: &Forks[4], outgoing: make(chan string), incoming: make(chan string), leftFree: true, rightFree: true}
-	var phil4 = Philosopher{number: 4, left: &Forks[4], right: &Forks[0], outgoing: make(chan string), incoming: make(chan string), leftFree: true, rightFree: true}
+	var phil0 = Philosopher{left: &fork0, right: &fork1, outgoing: make(chan int), incoming: make(chan int)}
+	var phil1 = Philosopher{left: &fork1, right: &fork2, outgoing: make(chan int), incoming: make(chan int)}
+	var phil2 = Philosopher{left: &fork2, right: &fork3, outgoing: make(chan int), incoming: make(chan int)}
+	var phil3 = Philosopher{left: &fork3, right: &fork4, outgoing: make(chan int), incoming: make(chan int)}
+	var phil4 = Philosopher{left: &fork0, right: &fork4, outgoing: make(chan int), incoming: make(chan int)} //Venstrehåndet
 
-	philosophers = []Philosopher{phil0, phil1, phil2, phil3, phil4}
-	fmt.Println("Philos init")
-	go phil0.receiver()
-	fmt.Println("Routine 1")
-	go phil1.receiver()
-	fmt.Println("Routine 2")
-	go phil2.receiver()
-	fmt.Println("Routine 3")
-	go phil3.receiver()
-	fmt.Println("Routine 4")
-	go phil4.receiver()
-	fmt.Println("Routine 5")
-	go Forks[0].forkReciever()
-	go Forks[1].forkReciever()
-	go Forks[2].forkReciever()
-	go Forks[3].forkReciever()
-	go Forks[4].forkReciever()
+	go phil0.eat()
+	go phil1.eat()
+	go phil2.eat()
+	go phil3.eat()
+	go phil4.eat()
 
-	fmt.Println("Routines started")
+	go fork0.forkReciever()
+	go fork1.forkReciever()
+	go fork2.forkReciever()
+	go fork3.forkReciever()
+	go fork4.forkReciever()
+
 	for {
 		select {
 		case msg1 := <-phil0.outgoing:
-			if msg1 == "Eating" {
-				fmt.Println("phil1 eating:" + strconv.Itoa(phil0.timesEaten))
-				fmt.Println("Fork 1 times used: " + strconv.Itoa(phil0.left.timesUsed))
-				fmt.Println("Fork 5 times used: " + strconv.Itoa(phil0.right.timesUsed))
+			if msg1 == -1 {
+				fmt.Println("Philosopher 1 is thinking")
 			} else {
-
+				fmt.Println("Philosopher 1 is eating " + strconv.Itoa(msg1))
 			}
 		case msg2 := <-phil1.outgoing:
-			if msg2 == "Eating" {
-				fmt.Println("phil2 eating: " + strconv.Itoa(phil1.timesEaten))
-				fmt.Println("Fork 2 times used: " + strconv.Itoa(phil1.left.timesUsed))
-				fmt.Println("Fork 1 times used: " + strconv.Itoa(phil1.right.timesUsed))
+			if msg2 == -1 {
+				fmt.Println("Philosopher 2 is thinking")
 			} else {
-
+				fmt.Println("Philosopher 2 is eating " + strconv.Itoa(msg2))
 			}
 
 		case msg3 := <-phil2.outgoing:
-			if msg3 == "Eating" {
-				fmt.Println("phil3 eating: " + strconv.Itoa(phil2.timesEaten))
-				fmt.Println("Fork 3 times used: " + strconv.Itoa(phil2.left.timesUsed))
-				fmt.Println("Fork 2 times used: " + strconv.Itoa(phil2.right.timesUsed))
+			if msg3 == -1 {
+				fmt.Println("Philosopher 3 is thinking")
 			} else {
-
+				fmt.Println("Philosopher 3 is eating " + strconv.Itoa(msg3))
 			}
 
 		case msg4 := <-phil3.outgoing:
-			if msg4 == "Eating" {
-				fmt.Println("phil4 eating: " + strconv.Itoa(phil3.timesEaten))
-				fmt.Println("Fork 4 times used: " + strconv.Itoa(phil3.left.timesUsed))
-				fmt.Println("Fork 3 times used: " + strconv.Itoa(phil3.right.timesUsed))
+			if msg4 == -1 {
+				fmt.Println("Philosopher 4 is thinking")
 			} else {
-
+				fmt.Println("Philosopher 4 is eating " + strconv.Itoa(msg4))
 			}
 
 		case msg5 := <-phil4.outgoing:
-			if msg5 == "Eating" {
-				fmt.Println("phil5 eating: " + strconv.Itoa(phil4.timesEaten))
-				fmt.Println("Fork 5 times used: " + strconv.Itoa(phil4.left.timesUsed))
-				fmt.Println("Fork 4 times used: " + strconv.Itoa(phil4.right.timesUsed))
+			if msg5 == -1 {
+				fmt.Println("Philosopher 5 is thinking")
 			} else {
-
+				fmt.Println("Philosopher 5 is eating " + strconv.Itoa(msg5))
 			}
-
+		case msg6 := <-fork0.outgoing:
+			fmt.Println("Fork 0 has been used " + strconv.Itoa(msg6) + " times")
+		case msg7 := <-fork1.outgoing:
+			fmt.Println("Fork 1 has been used " + strconv.Itoa(msg7) + " times")
+		case msg8 := <-fork2.outgoing:
+			fmt.Println("Fork 2 has been used " + strconv.Itoa(msg8) + " times")
+		case msg9 := <-fork3.outgoing:
+			fmt.Println("Fork 3 has been used " + strconv.Itoa(msg9) + " times")
+		case msg10 := <-fork4.outgoing:
+			fmt.Println("Fork 4 has been used " + strconv.Itoa(msg10) + " times")
 		}
 	}
-
-}
-
-func createForks() []Fork {
-	Forks := make([]Fork, 5)
-	for i := 0; i < 5; i++ {
-		Forks[i] = Fork{timesUsed: 0, outgoing: make(chan string), incoming: make(chan string), inUse: false}
-	}
-	return Forks
 }
