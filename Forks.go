683a71ffdede0bb05c37742969ad73100d739f21
Niclas Søgaard Hjortkjær <nihj@itu.dk>
2021-09-19 19:59:04 +0200

    version 1.0

--- Forks.go
+++ Forks.go
@@ -1,51 +1,25 @@
 package main
 
-import (
-	"strconv"
-	"sync"
-)
+import "sync"
 
 type Fork struct {
 	timesUsed int
-	inUse     bool
-	outgoing  chan string
-	incoming  chan string
+	outgoing  chan int
+	incoming  chan int
 	locker    sync.Mutex
 }
 
-func (f Fork) Lock() {
-	f.locker.Lock()
-	f.inUse = true
-	f.timesUsed++
-	f.outgoing <- "Lifted"
-}
-
-func (f Fork) Unlock() {
-	f.inUse = false
-	f.outgoing <- "Put down"
-	f.locker.Lock()
-}
-
 func (f Fork) forkReciever() {
+
 	for {
 		call := <-f.incoming
-		if call == "timesUsed" {
 
-			f.outgoing <- strconv.Itoa(f.timesUsed)
+		//Dette virker ligegyldigt, da der kun kan være en værdi i denne channel.
+		//Måske man ikke bør tage en værdi, men bare bede den om at inkremere timesUsed.
 
-		} else if call == "inUse" {
-
-			f.outgoing <- strconv.FormatBool(f.inUse)
-
-		} else if call == "lock" {
-
-			f.Lock()
-
-		} else if call == "unlock" {
-
-			f.Unlock()
-
+		if call == 1 {
+			f.timesUsed++
+			f.outgoing <- f.timesUsed
 		}
-
 	}
 }
--- Philosophers.go
+++ Philosophers.go
@@ -1,67 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"time"
 )
 
 type Philosopher struct {
 	timesEaten int
 	left       *Fork
-	leftFree   bool
 	right      *Fork
-	rightFree  bool
-	number     int
-	outgoing   chan string
-	incoming   chan string
+	outgoing   chan int
+	incoming   chan int
 }
 
 func (p Philosopher) eat() {
-	fmt.Println("eating")
-	p.left.incoming <- "lock"
-	p.right.incoming <- "lock"
-	p.outgoing <- "Eating"
-
-	philosophers[p.number-1%5].incoming <- "LeftNotFree"
-	philosophers[p.number+1%5].incoming <- "RightNotFree"
-
-	p.outgoing <- "Philosopher " + strconv.Itoa(p.number) + " has eaten " + strconv.Itoa(p.timesEaten)
-	time.Sleep(time.Millisecond * 500)
-	p.timesEaten++
-
-	p.left.incoming <- "unlock"
-	p.right.incoming <- "unlock"
-	p.outgoing <- "Thinking"
-
-	philosophers[p.number-1%5].incoming <- "LeftFree"
-	philosophers[p.number+1%5].incoming <- "RightFree"
-}
-
-func (p Philosopher) receiver() {
 	for {
-		fmt.Println("received1")
-		start := <-p.incoming
-		fmt.Println("received2")
-		if start == "LeftFree" {
-			p.leftFree = true
-		} else if start == "LeftNotFree" {
-			p.leftFree = false
-		} else if start == "RightFree" {
-			p.rightFree = true
-		} else if start == "RightNotFree" {
-			p.rightFree = false
-		} else if start == "timesEaten" {
-			p.outgoing <- strconv.Itoa(p.timesEaten)
-		}
-		fmt.Println("received")
-		p.eatIfPossible()
-	}
-}
 
-func (p Philosopher) eatIfPossible() {
-	fmt.Println("EatIfPossible")
-	if p.leftFree && p.rightFree {
-		p.eat()
+		p.right.locker.Lock()
+		p.left.locker.Lock()
+
+		// 1 betyder at nøglen skal starte
+		// Se note i Forks.go filen
+		p.right.incoming <- 1
+		p.left.incoming <- 1
+
+		p.timesEaten++
+		p.outgoing <- p.timesEaten
+
+		time.Sleep(time.Millisecond * 2000)
+
+		// Antallet af gange, der er spist kan aldrig være -1
+		// -1 betyder derfor at filosoffen er færdig med at spise
+		p.outgoing <- -1
+
+		p.right.locker.Unlock()
+		p.left.locker.Unlock()
+
+		time.Sleep(time.Millisecond * 500)
 	}
 }
