abdd449b3e606f150e837d9f9256392f9521d38b
Niclas Søgaard Hjortkjær <nihj@itu.dk>
2021-09-28 11:35:48 +0200

    version 2.0

--- Main.go
+++ Main.go
@@ -3,21 +3,22 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
+var fork0 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+var fork1 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+var fork2 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+var fork3 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+var fork4 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
+
+var phil0 = Philosopher{left: &fork0, right: &fork1, incoming: make(chan int), outgoing: make(chan int)}
+var phil1 = Philosopher{left: &fork1, right: &fork2, incoming: make(chan int), outgoing: make(chan int)}
+var phil2 = Philosopher{left: &fork2, right: &fork3, incoming: make(chan int), outgoing: make(chan int)}
+var phil3 = Philosopher{left: &fork3, right: &fork4, incoming: make(chan int), outgoing: make(chan int)}
+var phil4 = Philosopher{left: &fork0, right: &fork4, incoming: make(chan int), outgoing: make(chan int)} //Venstrehåndet
+
 func main() {
-
-	var fork0 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
-	var fork1 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
-	var fork2 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
-	var fork3 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
-	var fork4 = Fork{timesUsed: 0, outgoing: make(chan int), incoming: make(chan int)}
-
-	var phil0 = Philosopher{left: &fork0, right: &fork1, outgoing: make(chan int)}
-	var phil1 = Philosopher{left: &fork1, right: &fork2, outgoing: make(chan int)}
-	var phil2 = Philosopher{left: &fork2, right: &fork3, outgoing: make(chan int)}
-	var phil3 = Philosopher{left: &fork3, right: &fork4, outgoing: make(chan int)}
-	var phil4 = Philosopher{left: &fork0, right: &fork4, outgoing: make(chan int)} //Venstrehåndet
 
 	go phil0.eat()
 	go phil1.eat()
@@ -25,11 +26,19 @@ func main() {
 	go phil3.eat()
 	go phil4.eat()
 
+	go phil0.philReceiver()
+	go phil1.philReceiver()
+	go phil2.philReceiver()
+	go phil3.philReceiver()
+	go phil4.philReceiver()
+
 	go fork0.forkReciever()
 	go fork1.forkReciever()
 	go fork2.forkReciever()
 	go fork3.forkReciever()
 	go fork4.forkReciever()
+
+	go sendRequests()
 
 	for {
 		select {
@@ -77,5 +86,23 @@ func main() {
 		case msg10 := <-fork4.outgoing:
 			fmt.Println("Fork 4 has been used " + strconv.Itoa(msg10) + " times")
 		}
+	}
+}
+
+func sendRequests() {
+	for {
+		fmt.Println("-------------------------------------------")
+		phil0.incoming <- 1
+		phil1.incoming <- 1
+		phil2.incoming <- 1
+		phil3.incoming <- 1
+		phil4.incoming <- 1
+		fork0.incoming <- 1
+		fork1.incoming <- 1
+		fork2.incoming <- 1
+		fork3.incoming <- 1
+		fork4.incoming <- 1
+
+		time.Sleep(2500 * time.Millisecond)
 	}
 }
