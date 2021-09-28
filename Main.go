8a3b6d4a3aeb422d142289708bd525a5963778e0
Akongstad <kongstad@outlook.com>
2021-09-28 09:57:43 +0200

    Easier to read output

--- Main.go
+++ Main.go
@@ -35,36 +35,36 @@ func main() {
 		select {
 		case msg1 := <-phil0.outgoing:
 			if msg1 == -1 {
-				fmt.Println("Philosopher 1 is thinking")
+				fmt.Println("1 - Philosopher 1 is thinking")
 			} else {
-				fmt.Println("Philosopher 1 is eating " + strconv.Itoa(msg1))
+				fmt.Println("1 - Philosopher 1 is eating " + strconv.Itoa(msg1))
 			}
 		case msg2 := <-phil1.outgoing:
 			if msg2 == -1 {
-				fmt.Println("Philosopher 2 is thinking")
+				fmt.Println("2 - Philosopher 2 is thinking")
 			} else {
-				fmt.Println("Philosopher 2 is eating " + strconv.Itoa(msg2))
+				fmt.Println("2 - Philosopher 2 is eating " + strconv.Itoa(msg2))
 			}
 
 		case msg3 := <-phil2.outgoing:
 			if msg3 == -1 {
-				fmt.Println("Philosopher 3 is thinking")
+				fmt.Println("3 - Philosopher 3 is thinking")
 			} else {
-				fmt.Println("Philosopher 3 is eating " + strconv.Itoa(msg3))
+				fmt.Println("3 - Philosopher 3 is eating " + strconv.Itoa(msg3))
 			}
 
 		case msg4 := <-phil3.outgoing:
 			if msg4 == -1 {
-				fmt.Println("Philosopher 4 is thinking")
+				fmt.Println("4 - Philosopher 4 is thinking")
 			} else {
-				fmt.Println("Philosopher 4 is eating " + strconv.Itoa(msg4))
+				fmt.Println("4 - Philosopher 4 is eating " + strconv.Itoa(msg4))
 			}
 
 		case msg5 := <-phil4.outgoing:
 			if msg5 == -1 {
-				fmt.Println("Philosopher 5 is thinking")
+				fmt.Println("5 - Philosopher 5 is thinking")
 			} else {
-				fmt.Println("Philosopher 5 is eating " + strconv.Itoa(msg5))
+				fmt.Println("5 - Philosopher 5 is eating " + strconv.Itoa(msg5))
 			}
 		case msg6 := <-fork0.outgoing:
 			fmt.Println("Fork 0 has been used " + strconv.Itoa(msg6) + " times")
